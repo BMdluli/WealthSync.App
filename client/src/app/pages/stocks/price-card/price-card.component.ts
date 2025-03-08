f0d8250849cef6aa98c328a670c0e8a21f60a9b8
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-03-08 10:00:19 +0200

    move to laptop

---
+++ client/src/app/pages/stocks/price-card/price-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './price-card.component.html',
+  styleUrl: './price-card.component.scss'
+})
+export class PriceCardComponent {
+
+}
