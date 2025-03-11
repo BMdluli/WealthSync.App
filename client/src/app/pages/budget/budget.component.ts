4771daa7da1dca40ae3b9b19392452252e117dd1
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-03-11 10:26:03 +0200

    update: updated saving goal style

--- client/src/app/pages/budget/budget.component.ts
+++ client/src/app/pages/budget/budget.component.ts
@@ -1,19 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { HeaderComponent } from '../../header/header.component';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BudgetCardComponent } from './budget-card/budget-card.component';
 import { BudgetService } from '../../_services/budget.service';
 import { Budget } from '../../_models/budget';
 @Component({
   selector: 'app-budget',
   standalone: true,
-  imports: [
-    SidebarComponent,
-    HeaderComponent,
-    NgxChartsModule,
-    BudgetCardComponent,
-  ],
+  imports: [SidebarComponent, HeaderComponent, BudgetCardComponent],
   templateUrl: './budget.component.html',
   styleUrl: './budget.component.scss',
 })
--- client/src/app/savings-card/savings-card.component.ts
+++ client/src/app/savings-card/savings-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Goal } from '../_models/goal';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-savings-card',
   standalone: true,
-  imports: [NgCircleProgressModule],
+  imports: [NgCircleProgressModule, CommonModule],
   templateUrl: './savings-card.component.html',
   styleUrl: './savings-card.component.scss',
 })
@@ -18,6 +19,17 @@ export class SavingsCardComponent {
     startDate: new Date(),
     targetDate: new Date(),
   };
+
+  radius = 40; // Radius of the circle
+  circumference = 2 * Math.PI * this.radius; // Full circle length
+
+  get strokeDashoffset(): number {
+    // Offset based on calculated percentage
+    return (
+      this.circumference -
+      (this.calculatePercentage() / 100) * this.circumference
+    );
+  }
 
   calculatePercentage() {
     const percentage = (this.goal.currentAmount / this.goal.targetAmount) * 100;
