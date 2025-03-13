e6a79ea323a2fcfc00a67006e2470afbaae3d9f7
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-03-13 13:49:32 +0200

    update: added annual dividend income and removed unused sections

--- client/src/app/pages/stocks/stocks.component.ts
+++ client/src/app/pages/stocks/stocks.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { HeaderComponent } from '../../header/header.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
 import { Stock } from '../../_models/stock';
@@ -8,12 +7,12 @@ import { CommonModule } from '@angular/common';
 import { ModalService } from '../../_services/modal.service';
 import { CreateStockModalComponent } from '../../modals/create-stock-modal/create-stock-modal.component';
 import { LoaderComponent } from '../../loader/loader.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-stocks',
   standalone: true,
   imports: [
-    SidebarComponent,
     HeaderComponent,
     StockCardComponent,
     CommonModule,
@@ -25,14 +24,17 @@ import { LoaderComponent } from '../../loader/loader.component';
 })
 export class StocksComponent {
   stocks: Stock[] = [];
-  total: number = 5000;
+  total: number = 0;
+  gain: number = 0;
+  annualIncome: number = 0;
   isLoading = false;
 
   isModalOpen = false;
 
   constructor(
     private stocksService: StocksService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -40,21 +42,32 @@ export class StocksComponent {
     this.modalService.getModalState('stockModal').subscribe((isOpen) => {
       this.isModalOpen = isOpen;
     });
+    this.toastr.info(
+      'Please note that this works with AMERICAN COMPANIES and not ETFS or BONDS'
+    );
   }
 
   getStocks() {
     this.isLoading = true;
     this.stocksService.getStocks().subscribe({
-      next: (response) => (this.stocks = response),
+      next: (response) => {
+        this.stocks = response;
+        response.map((stock) => {
+          this.total += stock.shares * stock.currentPrice;
+          this.gain += stock.currentPrice - stock.purchasePrice;
+          this.annualIncome += stock.currentPrice * stock.dividendYield;
+        });
+      },
       error: (err) => console.error(err),
       complete: () => (this.isLoading = false),
     });
   }
 
   deleteStock(id: number) {
+    this.isLoading = true;
     this.stocksService.deleteStock(id).subscribe({
       next: (_) => {
-        console.log('deleted successfully');
+        this.toastr.success('stock deleted successfully');
         this.getStocks();
       },
       error: (err) => console.error(err),
@@ -82,17 +95,17 @@ export class StocksComponent {
     return avg;
   }
 
-  calculateAnnualIncome() {
-    let temp = 0;
-    if (this.stocks.length !== 0) {
-      this.stocks.map((stock) => (temp += stock.currentPrice));
-    }
-
-    return this.calculateSummaryValues() * temp;
-  }
-
   openModal() {
     this.modalService.openModal('stockModal');
-    // alert('Clicked');
+  }
+
+  getFrequency(frequency: string) {
+    if (frequency === 'Monthly') return 12;
+
+    if (frequency === 'Quarterly') return 4;
+
+    if (frequency === 'Semi-Annually') return 2;
+
+    return 4;
   }
 }
