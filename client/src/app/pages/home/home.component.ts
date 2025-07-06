84181f5f608074c313b8a34537f486ad7a9d6319
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2025-07-06 18:19:17 +0200

    update: changed landing page and updated budget links to make them look more like links

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  services = [
+    {
+      title: 'Keep Track stocks',
+      description: 'Keep track of stocks as well as the dividends they pay.',
+      imageUrl: 'assets/stocks-page.png',
+    },
+    {
+      title: 'Track Savings Goals',
+      description:
+        'Set up monthly budgets with budget categories to control your spending.',
+      imageUrl: 'assets/savings-page.png',
+    },
+    {
+      title: 'Track Budget',
+      description:
+        'Set goals that you want to save for and slowly add contributions towards them.',
+      imageUrl: 'assets/budget-page.png',
+    },
+  ];
+}
